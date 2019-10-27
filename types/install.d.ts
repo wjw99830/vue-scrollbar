f98f2835a1ee12b5f0aa4b01ce8f1566fa4f7a30
wangjiwei <wangjiwei@youzan.com>
2019-10-27 21:12:59 +0800

    fix: declaration files

--- types/install.d.ts
+++
@@ -1,6 +0,0 @@
-import { VueConstructor } from 'vue';
-import VueScrollbar from './VueScrollbar.vue';
-declare const plugin: {
-    install(Vue: VueConstructor<VueScrollbar>): void;
-};
-export default plugin;
